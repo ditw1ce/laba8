d880798c35f68ff718ac381c6a7b70ff625733ff
Ditw1ce <dimaykubovski@gmail.com>
2025-04-30 12:15:36 +0300

    добавлена структура MarathonRunner и конвертер времени

--- po8/marathon_runner.h
+++ po8/marathon_runner.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <string>
+
+struct MarathonRunner {
+    int id;
+    std::string surname;
+    std::string name;
+    std::string patronymic;
+    int start_time;
+    int finish_time;
+    std::string club;
+};
+
+int time_to_seconds(const std::string& time_str);
